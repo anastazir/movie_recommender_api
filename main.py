26dd9327a74acfa57c98a81f6d3bad5ec7fe3cd4
Anas Tazir <atazir2018@gmail.com>
2022-06-17 18:58:18 +0530

    import Recommender to main.py

--- main.py
+++ main.py
@@ -3,18 +3,11 @@ from flask_cors import CORS
 import requests
 import json
 
+from classes.Recommender import Recommender
+
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-
-
-@app.route('/about', methods=['GET'])
-def about():
-    return {'datasets_used': ["https://www.kaggle.com/datasets/rounakbanik/the-movies-dataset",
-                            "https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata"],
-            'type': 'Movie Recommender API',
-            'description': 'This is a movie recommender API system that uses the movie data from the Kaggle dataset.',
-            }
 
 @app.route('/fetch/<id>', methods=['GET', 'POST'])
 def fetch(id):
@@ -22,3 +15,15 @@ def fetch(id):
     data = requests.get(f"http://www.omdbapi.com/?i={id}&apikey=ac6564b1")
 
     return json.loads(data.text)
+
+@app.route('/recommend/<name_or_id>', methods=['GET', 'POST'])
+def recommend(name_or_id):
+    if str(name_or_id).startswith('tt') and len(str(name_or_id)) == 9:
+        type = "id"
+    else:
+        type = "title"
+    recommender = Recommender(type = type)
+
+    movies = recommender.get_recommendations(a = name_or_id)
+    json_res = movies.to_dict(orient = 'records')
+    return jsonify(json_res)
